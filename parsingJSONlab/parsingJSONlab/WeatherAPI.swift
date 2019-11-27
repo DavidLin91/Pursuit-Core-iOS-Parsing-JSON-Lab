06820bfd60dd5a9e173e1970e9f8b97a2aeeb680
David <lin.david985@gmail.com>
2019-11-27 12:35:04 -0500

    created tab bar and tableviews for each controller

---
+++ parsingJSONlab/parsingJSONlab/WeatherAPI.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  parsingJSONlab
+//
+//  Created by David Lin on 11/27/19.
+//  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
+//
+
+import UIKit
+
+class WeatherAPI: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
